7735620823b7ac1c92c632a2136c6ea74668355d
HaydenFaulkner <hay167167>
2019-08-18 20:55:03 +0930

    fix divide by 0

--- track.py
+++ track.py
@@ -184,7 +184,7 @@ def convert_x_to_bbox(x, score=None):
       [x1,y1,x2,y2] where x1,y1 is the top left and x2,y2 is the bottom right
     """
     w = np.sqrt(x[2] * x[3])
-    h = x[2] / w
+    h = x[2] / (w+.00000001)
     if (score == None):
         return np.array([x[0] - w / 2., x[1] - h / 2., x[0] + w / 2., x[1] + h / 2.]).reshape((1, 4))
     else:
